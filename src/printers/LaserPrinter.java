1ff79a36e8591ba1aeb54a9aca06e89ab43d94ac
bwielk <bwielk@gmail.com>
2017-08-12 16:24:35 +0200

    Laser printer design process

--- src/printers/LaserPrinter.java
+++ src/printers/LaserPrinter.java
@@ -1,9 +1,48 @@
 package printers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class LaserPrinter extends Printer{
+	
+	private HashMap<CMYK, LaserCartridge> cartridges;
 
 	public LaserPrinter(String brand, String model, int limit) {
 		super(brand, model, limit);
+		this.cartridges = new HashMap<CMYK, LaserCartridge>();
+
+	}
+	
+	public HashMap getCartridges(){
+		return this.cartridges;
+	}
+	
+	public String acceptCartridge(LaserCartridge cartridge){
+		if(cartridges.containsKey(cartridge.getColor())){
+			return "The " + cartridge.getColor() + " cartridge is already in";
+		}else{
+			cartridges.put(cartridge.getColor(), cartridge);
+			return "The " + cartridge.getColor() + " cartridge has been loaded";
+		}
+	}
+	
+	public String tonerReport(){
+		String cyan = (cartridges.containsKey(CMYK.CYAN))? "" + cartridges.get(CMYK.CYAN).getLevel()/200.0 * 100.0 + " %": "n/a";
+		String magenta = (cartridges.containsKey(CMYK.MAGENTA))? "" + cartridges.get(CMYK.MAGENTA).getLevel()/200.0*100.0 + " %": "n/a";
+		String yellow = (cartridges.containsKey(CMYK.YELLOW))? "" + cartridges.get(CMYK.YELLOW).getLevel()/200.0*100.0 + " %": "n/a";
+		String key = (cartridges.containsKey(CMYK.KEY))? "" + cartridges.get(CMYK.KEY).getLevel()/200.0*100.0 + " %": "n/a";
+		return "Toner levels : \nCYAN: " + cyan + " \nMAGENTA: " + magenta + " \nYELLOW: " + yellow + " \nKEY(BLACK): " + key + "";
+	}
+	
+	public String lowLevel() {
+		ArrayList<String> report = new ArrayList<String>();
+		for(LaserCartridge cartridge : cartridges.values()){
+			if(cartridge.getLevel() <= (double) 20.0){
+				report.add(cartridge.getColor().toString());
+			}
+		}
+		String summary = String.join(", ", report);
+		return "ATTENTION! The levels of inks: " + summary + " are low!";
 	}
 	
 	@Override
--- tests/printers/LaserPrinterTest.java
+++ tests/printers/LaserPrinterTest.java
@@ -7,14 +7,69 @@ import org.junit.Test;
 
 public class LaserPrinterTest{
 	
+	private LaserPrinter printer1;
+	private LaserCartridge cartridge1;
+	private LaserCartridge cartridge2;
+	private LaserCartridge cartridge3;
+	private LaserCartridge cartridge4;
+	
+	private void cartridgesIn(double a, double b, double c, double d){
+		cartridge1.setLevel(a);
+		cartridge2.setLevel(b);
+		cartridge3.setLevel(c);
+		cartridge4.setLevel(d);
+		printer1.acceptCartridge(cartridge1);
+		printer1.acceptCartridge(cartridge2);
+		printer1.acceptCartridge(cartridge3);
+		printer1.acceptCartridge(cartridge4);
+	}
+	
 	@Before
 	public void before(){
-		
+		printer1 = new LaserPrinter("Dell", "hdha", 200);
+		cartridge1 = new LaserCartridge(CMYK.CYAN);
+		cartridge2 = new LaserCartridge(CMYK.MAGENTA);
+		cartridge3 = new LaserCartridge(CMYK.YELLOW);
+		cartridge4 = new LaserCartridge(CMYK.KEY);
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void canGetCartridges(){
+		printer1.acceptCartridge(cartridge1);
+		assertEquals(1, printer1.getCartridges().size());
 	}
-
+	
+	@Test
+	public void canAccept4Cartridges(){
+		printer1.acceptCartridge(cartridge1);
+		printer1.acceptCartridge(cartridge2);
+		printer1.acceptCartridge(cartridge3);
+		printer1.acceptCartridge(cartridge4);
+		assertEquals(4, printer1.getCartridges().size());
+	}
+	
+	@Test
+	public void cannotAcceptTheSameCartridges(){
+		printer1.acceptCartridge(cartridge3);
+		assertEquals("The YELLOW cartridge is already in",printer1.acceptCartridge(cartridge3));
+	}
+	
+	@Test
+	public void checksTonerLevels(){
+		cartridgesIn(80.0, 100.00, 140.0, 160.0);
+		assertEquals("Toner levels : \nCYAN: 40.0 % \nMAGENTA: 50.0 % \nYELLOW: 70.0 % \nKEY(BLACK): 80.0 %", printer1.tonerReport());
+		System.out.println(printer1.tonerReport());
+	}
+	
+	@Test
+	public void checksTonerLevels2(){
+		cartridge1.setLevel(140.0);
+		cartridge2.setLevel(60.0);
+		printer1.acceptCartridge(cartridge1);
+		printer1.acceptCartridge(cartridge2);
+		assertEquals("Toner levels : \nCYAN: 70.0 % \nMAGENTA: 30.0 % \nYELLOW: n/a \nKEY(BLACK): n/a", printer1.tonerReport());
+		System.out.println(printer1.tonerReport());
+	}
+	
+	
 }
